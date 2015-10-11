f7dfc0817dd045421630edfa2dd3f1f6878f7022
joseph <yusufsaidcanbaz@gmail.com>
2015-10-11 12:14:39 +0300

    project source code added

---
+++ ImageCloner/Form1.Designer.cs
@@ -0,0 +1,199 @@
+﻿namespace ImageCloner
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.img_button = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.generate_button = new System.Windows.Forms.Button();
+            this.delete_button = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.flowLayoutPanel2 = new System.Windows.Forms.FlowLayoutPanel();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.add_button = new System.Windows.Forms.Button();
+            this.openFileDialog2 = new System.Windows.Forms.OpenFileDialog();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.flowLayoutPanel1.SuspendLayout();
+            this.flowLayoutPanel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 1;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.Controls.Add(this.flowLayoutPanel1, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.textBox1, 0, 2);
+            this.tableLayoutPanel1.Controls.Add(this.flowLayoutPanel2, 0, 1);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 3;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 41.1017F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 58.8983F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 24F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(922, 261);
+            this.tableLayoutPanel1.TabIndex = 0;
+            this.tableLayoutPanel1.Paint += new System.Windows.Forms.PaintEventHandler(this.tableLayoutPanel1_Paint);
+            // 
+            // flowLayoutPanel1
+            // 
+            this.flowLayoutPanel1.Controls.Add(this.img_button);
+            this.flowLayoutPanel1.Controls.Add(this.comboBox1);
+            this.flowLayoutPanel1.Controls.Add(this.generate_button);
+            this.flowLayoutPanel1.Controls.Add(this.delete_button);
+            this.flowLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(3, 3);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(916, 91);
+            this.flowLayoutPanel1.TabIndex = 0;
+            // 
+            // img_button
+            // 
+            this.img_button.Location = new System.Drawing.Point(3, 3);
+            this.img_button.Name = "img_button";
+            this.img_button.Size = new System.Drawing.Size(75, 23);
+            this.img_button.TabIndex = 1;
+            this.img_button.Text = "image";
+            this.img_button.UseVisualStyleBackColor = true;
+            this.img_button.Click += new System.EventHandler(this.img_button_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(84, 3);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(612, 21);
+            this.comboBox1.TabIndex = 2;
+            // 
+            // generate_button
+            // 
+            this.generate_button.Location = new System.Drawing.Point(702, 3);
+            this.generate_button.Name = "generate_button";
+            this.generate_button.Size = new System.Drawing.Size(75, 23);
+            this.generate_button.TabIndex = 3;
+            this.generate_button.Text = "generate";
+            this.generate_button.UseVisualStyleBackColor = true;
+            this.generate_button.Click += new System.EventHandler(this.generate_button_Click);
+            // 
+            // delete_button
+            // 
+            this.delete_button.Location = new System.Drawing.Point(783, 3);
+            this.delete_button.Name = "delete_button";
+            this.delete_button.Size = new System.Drawing.Size(75, 23);
+            this.delete_button.TabIndex = 3;
+            this.delete_button.Text = "delete";
+            this.delete_button.UseVisualStyleBackColor = true;
+            this.delete_button.Click += new System.EventHandler(this.delete_button_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.textBox1.Location = new System.Drawing.Point(3, 239);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(916, 20);
+            this.textBox1.TabIndex = 1;
+            // 
+            // flowLayoutPanel2
+            // 
+            this.flowLayoutPanel2.Controls.Add(this.textBox2);
+            this.flowLayoutPanel2.Controls.Add(this.textBox3);
+            this.flowLayoutPanel2.Controls.Add(this.add_button);
+            this.flowLayoutPanel2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.flowLayoutPanel2.FlowDirection = System.Windows.Forms.FlowDirection.TopDown;
+            this.flowLayoutPanel2.Location = new System.Drawing.Point(3, 100);
+            this.flowLayoutPanel2.Name = "flowLayoutPanel2";
+            this.flowLayoutPanel2.Size = new System.Drawing.Size(916, 133);
+            this.flowLayoutPanel2.TabIndex = 2;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(3, 3);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(864, 20);
+            this.textBox2.TabIndex = 0;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(3, 29);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(864, 20);
+            this.textBox3.TabIndex = 1;
+            // 
+            // add_button
+            // 
+            this.add_button.Location = new System.Drawing.Point(3, 55);
+            this.add_button.Name = "add_button";
+            this.add_button.Size = new System.Drawing.Size(75, 23);
+            this.add_button.TabIndex = 2;
+            this.add_button.Text = "add";
+            this.add_button.UseVisualStyleBackColor = true;
+            this.add_button.Click += new System.EventHandler(this.add_button_Click);
+            // 
+            // openFileDialog2
+            // 
+            this.openFileDialog2.Filter = "(*.png)|*.png";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(922, 261);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
+            this.flowLayoutPanel1.ResumeLayout(false);
+            this.flowLayoutPanel2.ResumeLayout(false);
+            this.flowLayoutPanel2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+        private System.Windows.Forms.Button img_button;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button generate_button;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog2;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel2;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Button add_button;
+        private System.Windows.Forms.Button delete_button;
+    }
+}
+
---
+++ ImageCloner/Form1.cs
@@ -0,0 +1,213 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace ImageCloner
+{
+    public partial class Form1 : Form
+    {
+        private string _img_file;
+        private List<string> _patterns = new List<string>();
+        private Image _img;
+        private Bitmap _bitmap;
+        
+        public Form1()
+        {
+            InitializeComponent();
+            bindSettings();
+        }
+
+        private void img_button_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog2.ShowDialog() == DialogResult.OK)
+            {
+                _img_file = openFileDialog2.FileName;
+                _img = Image.FromFile(_img_file);
+                _bitmap = new Bitmap(_img);
+                textBox1.Text = "image taken";
+            }
+        }
+
+        private void generate_button_Click(object sender, EventArgs e)
+        {
+            int ind = comboBox1.SelectedIndex;
+            int num_pattern = Properties.Settings.Default.num_pattern;
+
+            if (ind > -1 && num_pattern > ind)
+            {
+                textBox1.Text = "started to generate images";
+                string[] arr0 = Properties.Settings.Default.pattern_list.Split('-');
+                string pattern_str = arr0[ind];    
+                string[] arr2 = pattern_str.Split(' ');
+
+                for (int i = 1; i < arr2.Length; i++)
+                {
+                    string[] nums = arr2[i].Split('x');
+                    int wid = int.Parse(nums[0]);
+                    int hei = int.Parse(nums[1]);
+                    generateImg(wid, hei);
+                }
+            }
+            else
+            {
+                textBox1.Text = "index error";
+            }
+           
+        }
+
+        private void generateImg(int wid, int hei)
+        {
+            int original_wid = _img.Width;
+            int original_hei = _img.Height;
+
+            double original_ratio = original_wid / original_hei;
+            double ratio = wid / hei;
+
+            if (ratio == original_ratio)
+            {
+                resizeImage(_img, new Size(wid, hei)).Save(wid + "x" + hei + ".png");
+            }
+            else if (ratio > original_ratio)
+            {
+                int mid_hei = (int)(wid / original_ratio);
+                Image mid_img = resizeImage(_img, new Size(wid, mid_hei));
+                Bitmap mid_bitmap = new Bitmap(mid_img);
+                cropBitmap(mid_bitmap, 0, (mid_hei - hei) / 2, wid, hei).Save(wid + "x" + hei + ".png");
+            }
+            else
+            {
+                int mid_wid = (int)(hei * original_ratio);
+                Image mid_img = resizeImage(_img, new Size(mid_wid, hei));
+                Bitmap mid_bitmap = new Bitmap(mid_img);
+                cropBitmap(mid_bitmap, (mid_wid - wid) / 2, 0, wid, hei).Save(wid + "x" + hei + ".png");
+            }
+
+        }
+
+        private void tableLayoutPanel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void add_button_Click(object sender, EventArgs e)
+        {
+            string ptrn = textBox2.Text + " " + textBox3.Text;
+            addPattern(ptrn);
+            _patterns.Add(ptrn);
+            bindSettings();
+            textBox1.Text = "Restart Reqired!";
+        }
+
+        private void delete_button_Click(object sender, EventArgs e)
+        {
+            int ind = comboBox1.SelectedIndex;
+            if (ind > -1 && _patterns.Count > ind)
+            {
+                deletePattern(ind);
+                bindSettings();
+            }
+        }
+
+        private void bindSettings()
+        {
+            string str = Properties.Settings.Default.pattern_list;
+            string[] arr = str.Split('-');
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (arr[i] != "")
+                {
+                    _patterns.Add(arr[i]);
+                }
+            }
+            
+            comboBox1.DataSource = _patterns;
+            textBox1.Text = "Patterns binded";
+        }
+
+        private void addPattern(string pattern)
+        {
+            if (pattern != "" && pattern != null)
+            {
+                int num_pattern = Properties.Settings.Default.num_pattern;
+                Properties.Settings.Default.num_pattern += 1;
+                if (num_pattern > 0)
+                {
+                    Properties.Settings.Default.pattern_list += "-" + pattern;
+                }
+                else
+                {
+                    Properties.Settings.Default.pattern_list = pattern;
+                }
+                
+                Properties.Settings.Default.Save();
+                textBox1.Text = "Pattern added";
+            }
+        }
+
+        private void deletePattern(int ind)
+        {
+            if (ind > -1)
+            {
+                Properties.Settings.Default.num_pattern -= 1;
+                string[] arr = Properties.Settings.Default.pattern_list.Split('-');
+                string str = "";
+
+                for (int i = 0; i < arr.Length; i++)
+                {
+                    if (ind != i)
+                    {
+                        if (str == "")
+                        {
+                            str = arr[i];
+                        }
+                        else
+                        {
+                            str += "-" + arr[i];
+                        }
+                    }
+                }
+
+                Properties.Settings.Default.pattern_list = str;
+                Properties.Settings.Default.Save();
+            }
+            else
+            {
+                textBox1.Text = "Invalid index";
+            }
+        }
+                
+        private void refreshPatterns()
+        {
+            Properties.Settings.Default.num_pattern = 0;
+            Properties.Settings.Default.pattern_list = "";
+            Properties.Settings.Default.Save();
+
+            textBox1.Text = "Database refreshed";
+        }
+
+        public static Image resizeImage(Image imgToResize, Size size)
+        {
+            return (Image)(new Bitmap(imgToResize, size));
+        }
+
+        public Bitmap cropBitmap(Bitmap bitmap, int cropX, int cropY, int cropWidth, int cropHeight)
+        {
+            Rectangle rect = new Rectangle(cropX, cropY, cropWidth, cropHeight);
+            Bitmap cropped = bitmap.Clone(rect, System.Drawing.Imaging.PixelFormat.DontCare);
+            return cropped;
+        }
+
+        private void refreshSettings()
+        {
+            Properties.Settings.Default.num_pattern = 0;
+            Properties.Settings.Default.pattern_list = "";
+            Properties.Settings.Default.Save();
+        }
+    }
+}
